aa51d61aa1d1b2c0a0b3d3edf624fdd9d9f578af
BlaiseGratton <blaisegratton@gmail.com>
2015-02-07 20:46:41 -0600

    add peewee and table models

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
+from peewee import *
 
 app = Flask(__name__)
+
+database = SqliteDatabase('take_note_database')
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    password = CharField()
+    email = CharField()
+    join_date = DateTimeField()
+
+class Notes(BaseModel):
+    user = ForeignKeyField(User)
+    content = TextField()
+    pub_date = DateTimeField()
+
+    class Meta:
+        order_by = ('-pub_date',)
+
+def create_tables():
+    database.connect()
+    database.create_tables([User, Notes])
 
 @app.route('/<name>')
 def index(name="Colby"):
@@ -12,6 +37,11 @@ def notes(note1="CRUD is crud!", note2="I'm sad today", note3="Today I'm not!"):
     context = {'note1': note1, 'note2': note2, 'note3': note3}
     return render_template("notes.html", notes=context)
 
+@app.route('/notes/<note>')
+def note(note="Today I am sad"):
+    return render_template("note.html", note=note)
+
 # @app.route('/notes/<note>')
 
-app.run(debug=True, port=8000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='0.0.0.0')
