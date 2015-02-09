415c0561db3689cf76d92132ae47770e03c479f0
BlaiseGratton <blaisegratton@gmail.com>
2015-02-08 22:33:21 -0600

    add .gitignore, initialize db connections

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
-
+#! usr/bin/env python
+from flask import *  # Flask, render_template
 from peewee import *
 
 app = Flask(__name__)
 
-database = SqliteDatabase('take_note_database')
+DATABASE = 'take_note.db'
+
+database = SqliteDatabase(DATABASE, threadlocals=True)
 
 class BaseModel(Model):
     class Meta:
@@ -28,20 +30,34 @@ def create_tables():
     database.connect()
     database.create_tables([User, Notes])
 
+def get_user_notes(self):
+    return (User
+            .select()
+            .join(Notes, on=Notes.user)
+            .where(Notes.user == self.username))
+
+@app.before_request
+def before_request():
+    g.db = database
+    g.db.connect()
+
+@app.after_request
+def after_request():
+    g.db.close()
+    return response
+
 @app.route('/<name>')
-def index(name="Colby"):
+def index(name="user"):
     return render_template("index.html", name=name)
 
 @app.route('/notes')
-def notes(note1="CRUD is crud!", note2="I'm sad today", note3="Today I'm not!"):
-    context = {'note1': note1, 'note2': note2, 'note3': note3}
+def notes(user=User()):
+    context = get_user_notes(user)
     return render_template("notes.html", notes=context)
 
 @app.route('/notes/<note>')
 def note(note="Today I am sad"):
     return render_template("note.html", note=note)
-
-# @app.route('/notes/<note>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
