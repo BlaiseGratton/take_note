f159581bd543d641157bf7d54d1322c576126b1e
BlaiseGratton <blaisegratton@gmail.com>
2015-05-09 14:06:02 -0500

    moar form styling and dropdown snappiness

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,24 +1,29 @@
 jQuery(function($){
+  categoryDropdown = $('select[name="category"]');
 
-  $('#category-button').click(function(e){
-    $('#new-category').modal({
-      overlayClose:true,
-      opacity:80,
-      overlayCss: { backgroundColor: '#222' },
-      minHeight: 90
-    });
-    return false;
+  categoryDropdown.on('change', function(e){
+    if (this.value === "new-category") {
+      $('#new-category').modal({
+        overlayClose:true,
+        opacity:80,
+        overlayCss: { backgroundColor: '#222' },
+        minHeight: 90
+      });
+      this.value = "-Category-";
+      return false;
+    }
   });
 
   $('#submit-category').bind('click', function(){
     $.getJSON("/new_category", {
       name: $('#category-form>input[type="text"]').val()
     }, function(addedCategory){
-      $('select[name="category"]').append(
-        '<option value="' + addedCategory.id + '">' + addedCategory.name + '</option'
+      $('select[name="category"] option:last').before(
+        '<option selected="selected" value="' + addedCategory.id + '">' + addedCategory.name + '</option'
       );
       var success = $('<div>').text('Successfully added category').addClass('twelve columns notification success');
       $('.flashes').append(success);
+      categoryDropdown.value = addedCategory.id;
       $.modal.close();
     });
     return false;
